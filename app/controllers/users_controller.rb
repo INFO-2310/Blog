78436862a41dd63a3df46d05f428e5211a866681
Alex Meyers <ajm339@cornell.edu>
2014-09-16 01:25:13 -0400

    done users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+    #render json: User.all.map(&:name)
+    @user_names = User.all.map(&:name)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to @user
+  end
+
+  def user_params
+    params.require(:user).permit(:fname, :lname, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
   root 'posts#index'
+
   resources :posts
+  resources :users
+
 end

1b97e40dca7d3b462dc8b7b65b50790f3941504a
Feifan Zhou <feifan@me.com>
2014-09-17 14:44:09 -0400

    Login support (class 4)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
+  end
+
   def show
     @post = Post.find(params[:id])
   end
   
   def test
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end

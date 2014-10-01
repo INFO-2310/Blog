fc8b61c2a67c111fed067d50aae48d055a27a4aa
Feifan Zhou <feifan@me.com>
2014-10-01 00:19:02 -0400

    Add SessionsHelper to get Comments#create to work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,6 +23,7 @@ class CommentsController < ApplicationController
 
   # POST /comments
   # POST /comments.json
+  include SessionsHelper
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id

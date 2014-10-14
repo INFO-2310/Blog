70d60e3d65026e602ea36e74f4595c6fa0eca62f
Feifan Zhou <feifan@me.com>
2014-10-14 19:56:26 -0400

    Fix current_user to be more legible

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,8 @@
 module SessionsHelper
 
-  def current_user(*args)
-    x = Session.includes([:user => (args.nil? ? [] : args)]).find_by(:session_code => cookies[:session_code])
+  def current_user(options)
+    user_includes = options.has_key?(:includes) ? options[:includes] : []
+    x = Session.includes([user: user_includes]).find_by(session_code: cookies[:session_code])
    	@current_user = x.nil? ? nil : x.user
   end
 

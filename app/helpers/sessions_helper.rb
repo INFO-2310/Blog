78d980672d74f9e1c991c64438fcae334b358aa2
Feifan Zhou <feifan@me.com>
2014-10-14 22:52:23 -0400

    Make options an optional argument in current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
-  def current_user(options)
+  def current_user(options = {})
     user_includes = options.has_key?(:includes) ? options[:includes] : []
     x = Session.includes([user: user_includes]).find_by(session_code: cookies[:session_code])
    	@current_user = x.nil? ? nil : x.user
